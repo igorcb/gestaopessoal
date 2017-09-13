3a7208ccc50e3c0402479361f30c4818e955fc4c
Igor C. Batista <igor.batista@gmail.com>
2017-09-13 16:29:17 -0300

    create balance account_bank

--- app/models/account_bank.rb
+++ app/models/account_bank.rb
@@ -1,2 +1,7 @@
 class AccountBank < ActiveRecord::Base
+
+  def saldo(date=nil)
+    date.nil? ? CurrentAccount.where(account_bank_id: self.id).sum('price*type_launche') : CurrentAccount.where(account_bank_id: self.id, date_ocurrence: date).sum('price*type_launche')
+  end
+
 end

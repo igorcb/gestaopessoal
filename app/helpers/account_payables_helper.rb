2a72835d52d43b11a1db997c042a9fbe641461cf
Igor C. Batista <igor.batista@gmail.com>
2017-09-14 10:10:27 -0300

    swicht status pay

--- app/helpers/account_payables_helper.rb
+++ app/helpers/account_payables_helper.rb
@@ -1,7 +1,7 @@
 module AccountPayablesHelper
 
   def select_status_payables
-     ([["ABERTO", 0], ["PAGO PARCIAL", 1], ["PAGO", 2]])
+     ([["ABERTO", 0], ["PAGO", 1], ["PAGO PARCIAL", 2]])
   end
   
 end

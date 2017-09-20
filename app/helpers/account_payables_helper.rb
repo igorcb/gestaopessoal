d74795da95bd934383bda45d470a8569f5d086f9
Igor C. Batista <igor.batista@gmail.com>
2017-09-20 17:36:16 -0300

    add method sub_cost in helpers

--- app/helpers/account_payables_helper.rb
+++ app/helpers/account_payables_helper.rb
@@ -3,5 +3,13 @@ module AccountPayablesHelper
   def select_status_payables
      ([["ABERTO", 0], ["PAGO", 1], ["PAGO PARCIAL", 2]])
   end
-  
+
+  def sub_center(cost_center)
+    SubCostCenter.where("cost_center_id = ?", cost_center)
+  end
+
+  def sub_center_there(cost_center, sub_center)
+   sub = SubCostCenterThree.where("cost_center_id = ? and sub_cost_center_id = ? ", cost_center, sub_center)
+   sub.nil? ? 0 : sub
+  end
 end
